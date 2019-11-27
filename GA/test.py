7921466acacc8628564b2f53d933f94295fedf94
kunnapatt <folk_qq@hotmail.com>
2019-11-27 21:45:36 +0700

    Bug in fitness have same value

--- GA/test.py
+++ GA/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import copy
-import csv
+import random
 #### data | 1). ID Number, 2). Diagnosis (M = malignant, B = benign) -> Class 3). 3 -> 32 is features ####
 
 def main():
@@ -13,9 +13,13 @@ def main():
 
     neural = NeuralNetwork(input_x, true_x)
     neural.addLayer(5)
-    neural.addLayer(10)
+    # neural.addLayer(10)
+    # neural.addLayer(10)
+    # neural.addLayer(10)
+    # neural.addLayer(5)
+    # neural.addLayer(2)
     neural.addLayer(1)
-    neural.fit(2, 5)
+    neural.fit(100, 10)
     # a = neural.gene()
 
     # print(len(a[0]))
@@ -93,7 +97,9 @@ class NeuralNetwork:
         self.countsample = 0
         self.rememberweightT_2 = []
         self.individual = 0
-        self.chromosome = []
+        self.chromosome = {}
+        self.nchromosome = 0
+        self.child = []
         
     def addLayer(self, node):
         self.countLayer+=1
@@ -136,37 +142,130 @@ class NeuralNetwork:
         return out
 
     def fit(self, nchromosome, generation):
-        for i in range(nchromosome): # individual
-            self.chromosome.append([self.gene()]) # weight have dimension chromosome -> gene, fitness
-            out = self.FeedForward(self.chromosome[i][0])
-            self.Loss = []
-            for j in range(len(self.Fullinput[i])): # sample
+        self.nchromosome = nchromosome
+        for k in range(generation) :
+            fitne = []
+            self.chromosome = {}
+            for i in range(nchromosome): # individual
+                if k == 0 :
+                    temp = self.gene()
+                    temp = np.asarray(temp)
+                    out = self.FeedForward(temp)
+                else :
+                    # print(len(self.child))
+                    out = self.FeedForward(self.child[i])
 
-                err = out[j] - self.FullTrueOutput[j]
+                self.Loss = []
 
-                self.Loss.append(err)
+                for j in range(len(self.Fullinput[0])): # sample
 
-            mse = np.asarray(self.Loss)
-            mse = np.power(mse, 2)/ 2
-            mse = np.mean(mse) # scalar
-            fitness = self.fitness(mse)
-            self.chromosome[i].append(fitness)
-            print(self.chromosome[i][1])
+                    err = out[j] - self.FullTrueOutput[j]
+
+                    self.Loss.append(err)
+
+                mse = np.asarray(copy.deepcopy(self.Loss))
+                mse = np.power(mse, 2)/ 2
+                mse = np.mean(mse) # scalar
+                fitness = self.fitness(mse)
+                fitne.append(fitness)
+                self.chromosome[fitness] = {}
+                self.chromosome[fitness] = temp
+            
+            fitne = np.asarray(copy.deepcopy(fitne))
+            fitne = np.power(fitne, 2)/ 2
+            fitne = np.mean(fitne)
+            # print("Generation {} Fitness {}" .format(k+1, fitne))
+            self.chromosome = self.matingPool()
+            child = self.crossover(30)
+            self.child = self.mutate(child)
 
     def gene(self): # return list
         weight = []
         for  i in range(len(self.Node)):
             if (i == 0):
                 # weight.append(2*np.random.rand(len(self.Fullinput), self.Node[i]) - 1)
-                weight.append(np.random.uniform(0, 1, (len(self.Fullinput), self.Node[i])))
+                weight.append(np.random.uniform(-1, 1, (len(self.Fullinput), self.Node[i])))
             else : 
                 # weight.append(2*np.random.rand(self.Node[i-1], self.Node[i]) - 1)
-                weight.append(np.random.uniform(0, 1, (self.Node[i-1], self.Node[i])))
+                weight.append(np.random.uniform(-1, 1, (self.Node[i-1], self.Node[i])))
             
         return weight
 
     def fitness(self, mse): #### Cal fitness from error | input Integer | how to fitness maximum
         return 1/mse
+
+    def matingPool(self): # sort fitness value in chromosome
+        matingpool = {}
+
+        cou = 0
+        for i in sorted(self.chromosome.keys(), reverse=True) :
+
+            matingpool[i] = self.chromosome[i]
+            cou += 1
+
+        return matingpool
+
+    def crossover(self, nchild): #### return gene without fitness
+        child = [] # have only gene
+        for i in range(int(nchild/2)) :
+            a = random.choice(list(self.chromosome.keys()))
+            b = random.choice(list(self.chromosome.keys()))
+            
+            genea = []
+            geneb = []
+            for k in range(len(self.chromosome[a])):
+
+                ga = np.asarray(self.chromosome[a][k])
+                gb = np.asarray(self.chromosome[b][k])
+
+                shape_gene = ga.shape
+                genea_flatten = ga.flatten()
+                geneb_flatten = gb.flatten()
+
+                ncrossingpoint = np.random.randint(0, genea_flatten.shape[0])
+
+                list_crossingpoint = random.sample(range(genea_flatten.shape[0]), ncrossingpoint)
+
+                for j in list_crossingpoint :
+                    genea_flatten[j], geneb_flatten[j] = geneb_flatten[j], genea_flatten[j]
+
+                ga = np.reshape(genea_flatten, shape_gene)
+                gb = np.reshape(geneb_flatten, shape_gene)
+
+                genea.append(ga)
+                geneb.append(gb)
+
+            child.append(genea)
+            child.append(geneb)
+
+        if len(child) != self.nchromosome :
+            n_residual = self.nchromosome - len(child)
+            cou = 0
+            for i in self.chromosome.keys() :
+                if cou == n_residual :
+                    break
+                child.append(self.chromosome[i])
+                cou += 1
+
+        return child
+
+    def mutate(self, child):
+        child = copy.deepcopy(child)
+        n_chromosome = np.random.randint(len(child)) ### random n chromosome to mutate
+        choice_chromosome = random.sample(range(len(child)), n_chromosome) # random choice n chromosome to mutate
+        for i in range(len(choice_chromosome)):
+            for k in range(len(child[i])):
+                target = child[i][k].shape
+                gene = child[i][k].flatten()
+                n_gene = np.random.randint(gene.shape[0])
+                list_genemutate = random.sample(range(gene.shape[0]), n_gene)
+                list_mutate = np.random.uniform(-1, 1, n_gene)
+
+                for j in range(n_gene):
+                    gene[list_genemutate[j]] += list_mutate[j]
+                
+                child[i][k] = np.reshape(gene, target)
+        return child
 
 if __name__ == "__main__":
     main()
